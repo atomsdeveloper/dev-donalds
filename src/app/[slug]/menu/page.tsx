6ae3ace2b6b4f720c5f83954760594164717c19b
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-19 17:41:45 -0300

    feat: created products page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,47 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import RestaurantCategories from "./components/categories";
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumption_method: string }>;
+}
+
+const isConsumptionMethodValid = (consumption_method: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumption_method.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumption_method } = await searchParams;
+
+  if (!isConsumptionMethodValid(consumption_method)) {
+    return notFound;
+  }
+
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: {
+      menuCategories: {
+        include: { products: true },
+      },
+    },
+  });
+  if (!restaurant) {
+    return notFound;
+  }
+
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
+    </div>
+  );
+};
+export default RestaurantMenuPage;
