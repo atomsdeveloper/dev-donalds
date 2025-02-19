35f08cb68f8e8f62df638c58a13a54d62137eab4
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-19 17:32:57 -0300

    chore: config hostname to images and add images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+/* config options here */
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Configurando imagens para que o next aceite as imagens vindas desse host.
+  images: {
+    remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
+  },
 };
 
 export default nextConfig;
