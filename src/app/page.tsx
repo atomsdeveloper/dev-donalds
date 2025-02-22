9725132139d950c3477d1a300ea759decbbdf695
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-22 03:49:50 -0300

    fix: redirect home page to slug with restaurant exists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
+"use client"; // Certifique-se de que este componente é um componente do cliente
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const HomePage = () => {
-  return <div className=""> Home Page</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redireciona para a rota atual + /dev-donalds
+    router.push(`${window.location.pathname}/dev-donalds`);
+  }, [router]);
+
+  return <div className="">Home Page</div>;
 };
+
 export default HomePage;
